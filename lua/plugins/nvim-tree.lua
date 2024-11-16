e3766f9dc355709d45a2559092b4657c35e455d1
adefirmanf <syskeys32@gmail.com>
2024-11-16 17:43:25 +0700

    docs(nvim-tree plugins): add comment for main shortcut

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -1,12 +1,13 @@
 return {
- {
-  "nvim-tree/nvim-tree.lua",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  keys = {
-   { "<leader>e", "<cmd>NvimTreeToggle<cr>", desc = "Toggle NvimTree" },
+  {
+    "nvim-tree/nvim-tree.lua",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    keys = {
+      -- default mapping for add (a) , delete (d)
+      { "<leader>e", "<cmd>NvimTreeToggle<cr>", desc = "Toggle NvimTree" },
+    },
+    config = function()
+      require("nvim-tree").setup({})
+    end,
   },
-  config = function()
-   require("nvim-tree").setup({})
-  end,
- },
 }
